c09926f81c85257f99ebfe113fe71b1a364bdef0
Ashl3yV <127549843+Ashl3yV@users.noreply.github.com>
2023-08-29 01:58:27 -0600

    create page report

--- src/pages/indexReport.tsx
+++ src/pages/indexReport.tsx
@@ -1,62 +1,43 @@
 import React from 'react';
 import BaseLayout from "@/components/BaseLayout";
-import TableClient, { initialData, TableData } from '@/components/TableClient';
+import TableReport, { initialData, TableData } from '@/components/TableReport';
 import { TextField, InputAdornment } from '@mui/material';
 import Button from '@mui/material/Button';
-import { Search as SearchIcon } from '@mui/icons-material';
+import { Grade, Search as SearchIcon } from '@mui/icons-material';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import Graph from "@/components/GraphReport"
 
-export default function ClientPage() {
+export default function ReportPage() {
   const activeClientsCount = calculateActiveClientsCount();
   const deactiveClientsCount = calculateDeactivateClientsCount();
 
   return (
     <BaseLayout>
-
-      <div className='h'>
-        <div className='h1'>
-          <button className='btnClient'>+ Agregar Cliente</button>
-          <div className='Descarga'>
-            <TextField
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon className="SearchIcon" />
-                  </InputAdornment>
-                ),
-              }}
-              placeholder="Buscar cliente"
-              sx={{
-                width: '85%',
-                '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                  borderColor: '#2b8c8c',
-                },
-              }}
-            />
-            <Button variant="outlined"
-              startIcon={<CloudDownloadIcon className="custom-icon" />}
-              className="custom-button"
-            >
-              Excel
-            </Button>
-          </div>
+      <div className='container-report'>
+        <div className="Ganancias">
+          <img className="enabled" src="/img/money.png" />
+          <p className='text1'>{`${activeClientsCount}`}</p>
+          <h1 >Ganancias</h1>
         </div>
-
-
-        <div className="Habilitados">
+        <div className="Ingresos">
           <img className="enabled" src="/img/enabled.png" />
           <p className='text1'>{`${activeClientsCount}`}</p>
-          <h1 className='text2H'>Nuevos Clientes</h1>
+          <h1 className='text2H'>Ingresos</h1>
         </div>
 
-        <div className="Desabilitados">
+        <div className="Cancelar">
           <img className="disabled" src="/img/disabled.png" />
           <p className='text1'>{`${deactiveClientsCount}`}</p>
-          <h1 className='text2D'>Abandono De Clientes</h1>
+          <h1 className='text2D'>Desabilitados</h1>
         </div>
-      </div>
+        <div className="graph">
+          <Graph />
+        </div>
 
-      <TableClient data={initialData} />
+
+      </div>
+      <TableReport data={initialData} />
+
     </BaseLayout>
   );
 }
